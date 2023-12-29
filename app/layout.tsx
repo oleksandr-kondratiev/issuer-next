3653875fee17d3592e46e70adfc7803706a98715
Oleksandr Kondratiev <oleksandrkondratiev@Oleksandrs-MacBook-Air.local>
2023-12-29 14:52:11 +0200

    Setup Redix UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import NavBar from "./NavBar";
+import { Theme } from "@radix-ui/themes";
+import "@radix-ui/themes/styles.css";
 
 interface RootLayoutProps {
   children: React.ReactNode;
@@ -12,10 +14,12 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <header>
-          <NavBar />
-        </header>
-        <main>{children}</main>
+        <Theme>
+          <header>
+            <NavBar />
+          </header>
+          <main>{children}</main>
+        </Theme>
       </body>
     </html>
   );
