870c0f302c03efa86f82da153c662f462f05b01d
Oleksandr Kondratiev <oleksandrkondratiev@Oleksandrs-MacBook-Air.local>
2023-12-30 21:42:57 +0200

    Customize Radix UI Theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,24 @@
-import "./globals.css";
+import "@radix-ui/themes/styles.css";
 import { Inter } from "next/font/google";
 import NavBar from "./NavBar";
 import { Theme } from "@radix-ui/themes";
-import "@radix-ui/themes/styles.css";
+import "./globals.css";
+import "./theme-config.css";
 
 interface RootLayoutProps {
   children: React.ReactNode;
 }
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <Theme>
+      <body className={inter.variable}>
+        <Theme accentColor="ruby" radius="large" suppressHydrationWarning>
           <header>
             <NavBar />
           </header>
